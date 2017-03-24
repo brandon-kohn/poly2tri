5f233731781bd1457484b498a9bb415d72310351
Brandon Kohn <blkohn@hotmail.com>
2017-03-24 10:32:58 -0400

    Added versioning to poly2tri.

---
+++ poly2tri/version.hpp
@@ -0,0 +1,14 @@
+//
+//! Copyright © 2017
+//! Brandon Kohn
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+#ifndef POLY2TRI_VERSION_HPP
+#define POLY2TRI_VERSION_HPP
+
+#define POLY2TRI_VERSION "1.0.0"
+
+#endif//! POLY2TRI_VERSION_HPP
